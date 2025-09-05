862a26555f640852be3365c5463829744cb31a59
yubingu <37607464+ClimBin@users.noreply.github.com>
2025-09-05 16:00:22 +0800

    Add files via upload

---
+++ model/__init__.py
@@ -0,0 +1,6 @@
+# This file is used to mark the directory as a Python package.
+# You can also use this file to import specific classes or functions
+# from submodules for easier access.
+
+# Example:
+# from .module_name import ClassName, function_name
